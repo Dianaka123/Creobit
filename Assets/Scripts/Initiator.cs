1a32bc9d5eaf614576856e908dea83704256563c
Dianaka123 <diana.aristova.99@gmail.com>
2024-07-19 15:53:01 +0300

    Configure Firebase
    Ignore plugins for x86_64 because it's large.

--- Assets/Scripts/Initiator.cs
+++ Assets/Scripts/Initiator.cs
@@ -1,30 +1,31 @@
-//using Firebase;
-//using Firebase.Extensions;
+using Firebase;
+using Firebase.Extensions;
 using UnityEngine;
 
 public class Initiator : MonoBehaviour
 {
-    //private FirebaseApp app;
+    private FirebaseApp app;
     // Start is called before the first frame update
     void Start()
     {
-        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
-        //    var dependencyStatus = task.Result;
-        //    if (dependencyStatus == DependencyStatus.Available)
-        //    {
-        //        // Create and hold a reference to your FirebaseApp,
-        //        // where app is a Firebase.FirebaseApp property of your application class.
-        //        app = FirebaseApp.DefaultInstance;
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                app = FirebaseApp.Create();
 
-        //        // Set a flag here to indicate whether Firebase is ready to use by your app.
-        //    }
-        //    else
-        //    {
-        //        UnityEngine.Debug.LogError(string.Format(
-        //          "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-        //        // Firebase Unity SDK is not safe to use here.
-        //    }
-        //});
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(string.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
     }
 
     // Update is called once per frame
